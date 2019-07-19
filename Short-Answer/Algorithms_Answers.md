c932d3c0d48c2ad2835ac1fa92da329270a0e4ba
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-07-19 12:44:30 -0500

    Complete + stretch

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -1,1 +1,56 @@
 Add your answers to the Algorithms exercises here.
+
+a) a = 0
+\*\*\* a=0 Has a constant Runtime that is determined by the amount of times it runs at its worst performance. In this case a = 0 is O(1) \*\*\*
+
+    while (a < n * n * n): *** this is O(n**3) ***
+      a = a + n * n *** since we are assigning a to a + n * n, it only runs once ***
+
+    1 + 1 * n**3
+
+    ***we can pair the duplicates***
+    1 * n**3
+
+    *** only the biggest number matters ***
+
+    0(n**3)
+
+```
+b)  sum = 0 *** 0(1) *** (1)
+    for i in range(n):
+      i += 1 *** 0(1) *** (2)
+      for j in range(i + 1, n): (3)
+        j += 1 *** 0(1) *** (4)
+        for k in range(j + 1, n): (5)
+          k += 1 *** 0(1) *** (6)
+          for l in range(k + 1, 10 + k): (7)
+            l += 1 *** 0(1) *** (8)
+            sum += 1 *** 0(1) *** (9)
+
+```
+
+    \*\*\* sum = 0, i+= 1, j+= 1, k+= 1, l+= 1, Have a constant Runtime that is determined by the amount of times it runs at its worst performance. therefore they are all O(1) \*\*\*
+
+    As shown above, the last loop with run 9 times.
+    Each loop will run n - 1 times.
+
+    1 + n*(1 + (n - (i+1))*(1 + n - (j+1)) * 1
+
+    *** Can get get rid of the  * 1 because its just multiplying it by 1 ***
+
+     n*((n - (i+1))*(n - (j+1))
+    [1] [2]         [3]
+    *** only the biggest number matters ***
+
+    O(n**3)
+
+
+    c)  def bunnyEars(bunnies):
+      if bunnies == 0:
+        return 0
+
+      return 2 + bunnyEars(bunnies-1)
+
+      ***i think regardless of what is going on here, as in the above solutions, constants or assigned values always resolve to O(1)***
+
+    O(1)
--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -81,11 +81,13 @@ class SortingRobot:
         Turn on the robot's light
         """
         self._light = "ON"
+
     def set_light_off(self):
         """
         Turn off the robot's light
         """
         self._light = "OFF"
+
     def light_is_on(self):
         """
         Returns True if the robot's light is on and False otherwise.
@@ -97,14 +99,44 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        self.swap_item()  # initiates it to swap
+        self.move_right()  # then to pass it to the right
+        while self.light_is_on() == False:  # when the light is off
+            while self.can_move_right():  # and when we can move things to the right
+                if self.compare_item() == -1:  # if the item in the right hand is less than the item in front of it
+                    self.swap_item()  # swap it
+                self.move_right()  # then move it to the right
+            if self.compare_item() == 1:  # if the item in the right hand is more than the item in front of it
+                self.swap_item()  # swap it
+            self.move_left()  # move it left
+            while self.can_move_left():  # and when we can move things to the left
+                if self.compare_item() == 1:  # if the item in the right hand is more than the item in front of it
+                    self.swap_item()  # swap it
+                    self.move_left()  # move it left
+
+                if self.compare_item() == -1 or self.compare_item() == 0:
+                    # if the item in the left hand is less or equal than the item in front of it
+                    self.move_left()  # swap it
+                if self.compare_item() == None:  # if the item in the left hand is none
+                    break
+
+            self.swap_item()
+            self.move_right()
+            self.swap_item()
+            # if the item in the right hand is none this indicates the end of the line and the process is complete
+            if self.can_move_right() == False and self.compare_item() == None:
+                self.swap_item()  # final swap
+                self.set_light_on()  # FLASHING LIGHTS EVERYWHERE
+
+        return self._list
 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1,
+         45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
     robot = SortingRobot(l)
 
